f8f500a90350634872dc069c858adeff5b4c333b
MSalopek <matija.salopek994@gmail.com>
2024-04-27 21:19:07 +0200

    create storechanges plugin; update README.md

--- README.md
+++ README.md
@@ -1,2 +1,111 @@
 # cosmos-plugins
 cosmos-sdk plugins for versions >= 0.50.x
+
+## storechanges (blockheader, KV store changes)
+
+Implements `ABCIListener`.
+
+Allows saving `StoreKVPair` and the cometBFT header to a file for further processing.
+```go
+type ABCIListener interface {
+	ListenFinalizeBlock(ctx context.Context, req abci.RequestFinalizeBlock, res abci.ResponseFinalizeBlock) error
+	ListenCommit(ctx context.Context, res abci.ResponseCommit, changeSet []*StoreKVPair) error
+}
+```
+
+### Usage
+
+You must compile the plugin before use and configure your cosmos-sdk node (`simd` or other) to use your plugin.
+
+```shell
+## build
+cd cosmos-plugins
+go build -o storechanges storechanges.go
+
+# move it if you want
+# mv storechanges <path>
+
+## export path to plugin so the chain app can find it
+export COSMOS_SDK_ABCI="<abs_path_to_parent_dir>/storechanges"
+```
+
+Configuring your node:
+In your `app.toml` add these sections:
+```toml
+[streaming]
+
+[streaming.abci]
+
+# List of kv store keys to stream out via gRPC.
+# The store key names MUST match the module's StoreKey name.
+#
+# Example:
+# ["acc", "bank", "gov", "staking", "mint"[,...]]
+# ["*"] to expose all keys.
+keys = ["bank", "transfer"]
+
+plugin = "abci"
+
+# stop-node-on-err specifies whether to stop the node on message delivery error.
+stop-node-on-err = true
+```
+
+
+### Plugin configuration
+
+Plugin is configured using environment variables. Here are the available options:
+```shell
+# check .envexample
+COMET_RPC_URL = "tcp://localhost:26657" # comet node used for fetching block data 
+STREAMING_DIR = "~/.storechanges" # output files will be stored to this directory
+```
+
+The listed values are also used as defaults.
+
+### Start node and plugins
+Create a an env file before starting your node and export the variables.
+```shell
+cp .env.example .env
+# make changes
+```
+
+Export the variables before starting your node (in the same terminal session) and start your node.
+```shell
+set -a
+. .env
+set +a
+
+simd start
+```
+
+### App configuration
+
+If your node does not register streaming listeners you will need to add this snippet to your `app.go`:
+```diff
+// app/app.go
+func NewSimApp(
+	logger log.Logger,
+	db dbm.DB,
+	traceStore io.Writer,
+	loadLatest bool,
+	appOpts servertypes.AppOptions,
+	baseAppOptions ...func(*baseapp.BaseApp),
+) *SimApp {
+    // ...
+
+	keys := storetypes.NewKVStoreKeys(
+		authtypes.StoreKey, banktypes.StoreKey, stakingtypes.StoreKey, crisistypes.StoreKey,
+		minttypes.StoreKey, distrtypes.StoreKey, slashingtypes.StoreKey,
+		govtypes.StoreKey, paramstypes.StoreKey, consensusparamtypes.StoreKey, upgradetypes.StoreKey, feegrant.StoreKey,
+		evidencetypes.StoreKey, circuittypes.StoreKey,
+		authzkeeper.StoreKey, nftkeeper.StoreKey, group.StoreKey,
+	)
+
++	// register streaming services
++	if err := bApp.RegisterStreamingServices(appOpts, keys); err != nil {
++		panic(err)
++	}
+    
+    // ...
+}
+```
---
+++ cmd/storechanges/main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path"
+
+	abci "github.com/cometbft/cometbft/abci/types"
+	"github.com/hashicorp/go-plugin"
+
+	"cosmossdk.io/errors"
+	streamingabci "cosmossdk.io/store/streaming/abci"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	svc "github.com/cosmos/cosmos-sdk/client/grpc/cmtservice"
+
+	store "cosmossdk.io/store/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+)
+
+type FilePlugin struct {
+	BlockHeight  int64
+	Cdc          codec.Codec
+	CometClient  *client.Context
+	StreamingDir string
+}
+
+func (a *FilePlugin) ListenFinalizeBlock(ctx context.Context, req abci.RequestFinalizeBlock, res abci.ResponseFinalizeBlock) error {
+	a.BlockHeight = req.Height
+	return nil
+}
+
+func (a *FilePlugin) ListenCommit(ctx context.Context, res abci.ResponseCommit, changeSet []*store.StoreKVPair) error {
+	_, block, err := svc.GetProtoBlock(ctx, *a.CometClient, &a.BlockHeight)
+	if err != nil {
+		return err
+	}
+
+	bz, err := a.Cdc.Marshal(&block.Header)
+	if err != nil {
+		return err
+	}
+
+	if err := writeLengthPrefixedFile(fmt.Sprintf("block-%d-header", block.Header.Height), bz); err != nil {
+		return err
+	}
+
+	var blockDataBuf bytes.Buffer
+	if err := a.writeBlockData(&blockDataBuf, changeSet); err != nil {
+		return err
+	}
+
+	if err := writeLengthPrefixedFile(fmt.Sprintf("block-%d-data", block.Header.Height), blockDataBuf.Bytes()); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *FilePlugin) writeBlockData(writer io.Writer, kps []*store.StoreKVPair) error {
+	for i := range kps {
+		bz, err := a.Cdc.MarshalLengthPrefixed(kps[i])
+		if err != nil {
+			return err
+		}
+
+		if _, err := writer.Write(bz); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func writeLengthPrefixedFile(fname string, data []byte) (err error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	streamingDir := fmt.Sprintf("%s/.cosmos-streaming/dydx", home)
+	fpath := fmt.Sprintf("%s/%s", streamingDir, fname)
+
+	var f *os.File
+	f, err = os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o600)
+	if err != nil {
+		return errors.Wrapf(err, "open file failed: %s", fpath)
+	}
+
+	defer func() {
+		// avoid overriding the real error with file close error
+		if err1 := f.Close(); err1 != nil && err == nil {
+			err = errors.Wrapf(err, "close file failed: %s", fpath)
+		}
+	}()
+	_, err = f.Write(store.Uint64ToBigEndian(uint64(len(data))))
+	if err != nil {
+		return errors.Wrapf(err, "write length prefix failed: %s", fpath)
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		return errors.Wrapf(err, "write block data failed: %s", fpath)
+	}
+
+	return err
+}
+
+func main() {
+	cometRPC := os.Getenv("COMET_RPC_URL")
+	if cometRPC == "" {
+		cometRPC = "tcp://localhost:26657"
+	}
+
+	streamingDir := os.Getenv("STREAMING_DIR")
+	if streamingDir == "" {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			panic(fmt.Sprintf("error getting home directory: %s", err))
+		}
+		streamingDir = path.Join(home, ".storechanges")
+		err = os.Mkdir(streamingDir, 0o755)
+		if err != nil {
+			panic(fmt.Sprintf("error creating streaming directory: %s", err))
+		}
+	}
+
+	reg := codectypes.NewInterfaceRegistry()
+	cdc := codec.NewProtoCodec(reg)
+
+	// connect comet client for getting block data
+	clientCtx := client.Context{}
+	rpcclient, err := client.NewClientFromNode(cometRPC)
+	if err != nil {
+		panic(fmt.Sprintf("error getting comet client: %s", err))
+	}
+	clientCtx = clientCtx.WithClient(rpcclient)
+
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: streamingabci.Handshake,
+		Plugins: map[string]plugin.Plugin{
+			"abci": &streamingabci.ListenerGRPCPlugin{
+				Impl: &FilePlugin{
+					Cdc:          cdc,
+					CometClient:  &clientCtx,
+					StreamingDir: streamingDir,
+				},
+			},
+		},
+		GRPCServer: plugin.DefaultGRPCServer,
+	})
+}
